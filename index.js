2c30de1b522cd17bea5648953655df25f6ff3f12
alice007 <alice.moradiya@gmail.com>
2023-11-06 01:22:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -195,61 +195,6 @@ async function InsertionSort(array) {
   return array;
 }
 
-//write heap sort function
-
-async function HeapSort(array) {
-  let bars = document.getElementsByClassName("bar");
-  for (let i = Math.floor(array.length / 2); i >= 0; i--) {
-    await heapify(array, array.length, i);
-  }
-  for (let i = array.length - 1; i >= 0; i--) {
-    await swap(array, 0, i, bars);
-    await heapify(array, i, 0);
-  }
-  for (let k = 0; k < bars.length; k++) {
-    bars[k].style.backgroundColor = "aqua";
-    await sleep(speedFactor);
-  }
-  return array;
-}
-
-async function heapify(array, n, i) {
-  let bars = document.getElementsByClassName("bar");
-  let largest = i;
-  let left = 2 * i + 1;
-  let right = 2 * i + 2;
-  if (left < n && array[left] > array[largest]) {
-    largest = left;
-  }
-  if (right < n && array[right] > array[largest]) {
-    largest = right;
-  }
-  if (largest != i) {
-    await swap(array, i, largest, bars);
-    await heapify(array, n, largest);
-  }
-}
-
-async function swap(array, i, j, bars) {
-  let temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
-  bars[i].style.height = array[i] * heightFactor + "px";
-  bars[j].style.height = array[j] * heightFactor + "px";
-  bars[i].style.backgroundColor = "red";
-  bars[j].style.backgroundColor = "red";
-  await sleep(speedFactor);
-
-  for (let k = 0; k < bars.length; k++) {
-    if (k != i && k != j) {
-      bars[k].style.backgroundColor = "aqua";
-    }
-  }
-  //bars[i].innerText = array[i];
-  //bars[j].innerText = array[j];
-  return array;
-}
-
 //write mergeSort function
 async function mergeSort(arr) {
   let bars = document.getElementsByClassName("bar");
